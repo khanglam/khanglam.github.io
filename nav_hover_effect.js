0c162836299aaa4944feb8379abc68337a4f0a03
khanglam <khangtlam@gmail.com>
2018-11-01 23:41:08 -0700

    Fixed Nav Menu
    Finished Footer, Fixed Nav Menu

--- nav_hover_effect.js
+++ nav_hover_effect.js
@@ -1,10 +1,10 @@
-function hoverEffect(selected) {
+function NOTWORKING_hoverEffect(selected) {//Not actually used at all... Hover effect is handled using CSS, refer to #nav ul:hover
 
     //Apparently we can't use selected.style.color to grab the color if its not directly defined in html file,
     //if the color is defined in CSS then selected.style.color will return null... refer to <a> color in html file
     var home = document.getElementById("home");
     var about = document.getElementById("about");
-    var contact = document.getElementById("contact");
+    var portfolio = document.getElementById("portfolio");
     var resume = document.getElementById("resume");
 
 
@@ -17,19 +17,56 @@ function hoverEffect(selected) {
         about.style.color = "grey";
     else if(about.style.color == "grey")
         about.style.color = "white";
-    if(contact.style.color == "white")
-        contact.style.color = "grey";
-    else if(contact.style.color == "grey")
-    contact.style.color = "white";
+    if(portfolio.style.color == "white")
+        portfolio.style.color = "grey";
+    else if(portfolio.style.color == "grey")
+        portfolio.style.color = "white";
     if(resume.style.color == "white")
         resume.style.color = "grey";
     else if(resume.style.color == "grey")
         resume.style.color = "white";
 
 
-    selected.style.color = "white";
+        selected.style.color = "white";
 }
 
-function hoverOverProject(selected){
-    selected.style.background = "black";
+
+function mouseOn() {
+    document.getElementById("home").style.color = "grey";
+    document.getElementById("about").style.color = "grey";
+    document.getElementById("portfolio").style.color = "grey";
+    document.getElementById("resume").style.color = "grey";
+
+}
+
+function mouseOut() {
+    document.getElementById("home").style.color = "white";
+    document.getElementById("about").style.color = "white";
+    document.getElementById("portfolio").style.color = "white";
+    document.getElementById("resume").style.color = "white";
+}
+
+function hoverNavigation() {
+    var home = document.getElementById("home");
+    var about = document.getElementById("about");
+    var portfolio = document.getElementById("portfolio");
+    var resume = document.getElementById("resume");
+
+
+    if (home.style.color == "white")
+        home.style.color = "grey";
+    else if (home.style.color == "grey")
+        home.style.color = "white";
+    if (about.style.color == "white")
+        about.style.color = "grey";
+    else if (about.style.color == "grey")
+        about.style.color = "white";
+    if (portfolio.style.color == "white")
+        portfolio.style.color = "grey";
+    else if (portfolio.style.color == "grey")
+        portfolio.style.color = "white";
+    if (resume.style.color == "white")
+        resume.style.color = "grey";
+    else if (resume.style.color == "grey")
+        resume.style.color = "white";
 }
