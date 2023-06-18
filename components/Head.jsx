42c8ec78404d40cc848242175c5c3a81f3117aaf
Khang Lam <khangtlam@gmail.com>
2023-06-18 16:00:38 -0700

    Added Project conents

--- components/Head.jsx
+++ components/Head.jsx
@@ -1,24 +1,15 @@
-import Head from 'next/head';
+import Head from "next/head";
 
 const CustomHead = ({ title }) => {
   return (
     <Head>
       <title>{title}</title>
-      <meta
-        name="description"
-        content="Nitin Ranganath is an avid full stack web developer building websites and applications you'd love to use"
-      />
-      <meta
-        name="keywords"
-        content="nitin ranganath, nitin, ranganath, web developer portfolio, nitin web developer, nitin developer, mern stack, nitin ranganath portfolio, vscode-portfolio"
-      />
-      <meta property="og:title" content="Nitin Ranganath's Portfolio" />
-      <meta
-        property="og:description"
-        content="A full-stack developer building websites that you'd like to use."
-      />
-      <meta property="og:image" content="https://imgur.com/4zi5KkQ.png" />
-      <meta property="og:url" content="https://vscode-portfolio.vercel.app" />
+      <meta name="description" content="" />
+      <meta name="keywords" content="" />
+      <meta property="og:title" content="Portfolio" />
+      <meta property="og:description" content="" />
+      <meta property="og:image" content="" />
+      <meta property="og:url" content="" />
       <meta name="twitter:card" content="summary_large_image" />
     </Head>
   );
@@ -27,5 +18,5 @@ const CustomHead = ({ title }) => {
 export default CustomHead;
 
 CustomHead.defaultProps = {
-  title: 'Nitin Ranganath',
+  title: "Khang Lam",
 };
--- pages/projects.jsx
+++ pages/projects.jsx
@@ -1,15 +1,21 @@
-import ProjectCard from '../components/ProjectCard';
-import { getProjects } from './api/projects';
-import styles from '../styles/ProjectsPage.module.css';
+import ProjectCard from "../components/ProjectCard";
+import { getProjects } from "./api/projects";
+import styles from "../styles/ProjectsPage.module.css";
+import Background from "../styles/FloatingLogosBackground";
 
 const ProjectsPage = ({ projects }) => {
   return (
     <>
-      <h3>Recent Projects</h3>
-      <div className={styles.container}>
-        {projects.map((project) => (
-          <ProjectCard key={project.id} project={project} />
-        ))}
+      <div className={styles.parent_container}>
+        <div className={styles.background}>
+          <Background />
+        </div>
+        <h3 style={{ zIndex: 2 }}>Recent Projects</h3>
+        <div className={styles.container}>
+          {projects.map((project) => (
+            <ProjectCard key={project.id} project={project} />
+          ))}
+        </div>
       </div>
     </>
   );
@@ -19,7 +25,7 @@ export async function getStaticProps() {
   const projects = getProjects();
 
   return {
-    props: { title: 'Projects', projects }
+    props: { title: "Projects", projects },
   };
 }
 
