6e89dafb470a4e618ef3bc43082e980d7abc5a5f
Khang Lam <khangtlam@gmail.com>
2024-06-09 19:35:03 -0700

    Update about.jsx

--- pages/about.jsx
+++ pages/about.jsx
@@ -22,9 +22,20 @@ const AboutPage = () => {
             zIndex: 2
           }}
         >
-          <h3>
-Passionate and skilled Software Developer with a strong foundation in Java, Python, and JavaScript, and over 5 years of experience in software automation. Demonstrated expertise in automated testing and agile methodologies, with hands-on full-stack development experience through personal and academic projects. Proven track record of optimizing processes and enhancing code quality. Seeking to leverage my technical skills and project experience to contribute to innovative software development projects at a dynamic tech company.
-          </h3>
+<h3>Welcome to My Portfolio!</h3>
+            
+Hello there! Welcome to my digital space—it's a pleasure to have you here.
+
+<h3>About Me</h3>
+I'm a Software Developer on a mission to craft innovative solutions that drive real-world impact. With over 5 years of experience under my belt, I've mastered a diverse set of programming languages, including Java, Python, and JavaScript, alongside foundational knowledge in C++.
+
+<h3>What Sets Me Apart</h3>
+In the dynamic world of software automation, I've honed my skills in a plethora of cutting-edge tools and methodologies. From harnessing the power of Git for version control to orchestrating seamless testing processes with Selenium, I've embraced the latest technologies to streamline development workflows. However, my true passion lies in software development. I'm excited to transition my expertise from testing to full-fledged development, bringing with me a wealth of experience in optimizing processes and enhancing code quality.
+<h3>My Mission</h3>
+I'm on a mission to drive innovation and deliver excellence in every project I undertake. Take, for example, my project Youtube2Spotify—a testament to my ability to leverage APIs and authentication protocols to create seamless user experiences. With a tech stack comprising JavaScript, React, and Python, I've demonstrated my capacity to tackle complex challenges head-on and deliver tangible results.
+
+<h3>Let's Connect!</h3>
+Are you in search of a passionate Software Developer with a proven track record of success? Look no further—I'm here to bring value to your team. Reach out today, and let's explore how we can collaborate on your next groundbreaking project.          
           <h3>
             Resume{'  ('}
             <a
